d8a9b5404224748586121f8cc3e31e72efb2e6a8
Linn1024 <linn1024@gmail.com>
2012-09-27 23:00:51 +0300

    Update ITMOPrelude/Primitive.hs

--- ITMOPrelude/Primitive.hs
+++ ITMOPrelude/Primitive.hs
@@ -86,7 +86,10 @@ natOne = Succ Zero -- 1
 
 -- Сравнивает два натуральных числа
 natCmp :: Nat -> Nat -> Tri
-natCmp = undefined
+natCmp Zero Zero  = EQ
+natCmp Zero (Succ _) = LT
+natCmp (Succ _) Zero = GT
+natCmp (Succ n) (Succ m) = natCmp n m
 
 -- n совпадает с m 
 natEq :: Nat -> Nat -> Bool
@@ -111,7 +114,9 @@ Zero     +. m = m
 infixl 6 -.
 -- Вычитание для натуральных чисел
 (-.) :: Nat -> Nat -> Nat
-n -. m = undefined
+Zero -. _ = Zero
+n -. Zero = n
+(Succ n) -. (Succ m) = n -. m
 
 infixl 7 *.
 -- Умножение для натуральных чисел
@@ -119,52 +124,113 @@ infixl 7 *.
 Zero     *. m = Zero
 (Succ n) *. m = m +. (n *. m)
 
+natDiv :: Nat -> Nat -> Nat
+natDiv Zero Zero = undefined
+natDiv n Zero = undefined
+natDiv Zero n = Zero
+natDiv n m = (if' (natLt n m) Zero (natDiv(n -. m) m))
+
+
+natMod :: Nat -> Nat -> Nat
+natMod Zero Zero = undefined
+natMod n Zero = undefined
+natMod Zero n = Zero
+natMod n m = (if' (natLt n m) m (natDiv(n -. m) m))
+
+
+
+
 -- Целое и остаток от деления n на m
 natDivMod :: Nat -> Nat -> Pair Nat Nat
-natDivMod n m = undefined
+natDivMod n m = Pair (natDiv n m) (natMod n m)
 
-natDiv n = fst . natDivMod n -- Целое
-natMod n = snd . natDivMod n -- Остаток
 
 -- Поиск GCD алгоритмом Евклида (должен занимать 2 (вычислителельная часть) + 1 (тип) строчки)
 gcd :: Nat -> Nat -> Nat
-gcd = undefined
+gcd Zero n = n
+gcd n m = gcd(natMod m n) m
 
 -------------------------------------------
 -- Целые числа
 
 -- Требуется, чтобы представление каждого числа было единственным
-data Int = UNDEFINED deriving (Show,Read)
+data Int = Pos Nat| Neg Nat deriving (Show,Read)
 
-intZero   = undefined   -- 0
-intOne    = undefined     -- 1
-intNegOne = undefined -- -1
+intZero   = Pos natZero  -- 0
+intOne    = Pos natOne     -- 1
+intNegOne = Neg natZero -- -1
+
+
+intAbs :: Int -> Nat
+intAbs (Pos a) = a
+intAbs (Neg a) = a+.natOne
+
+intSign :: Int -> Nat
+intSign (Pos a) = natZero
+intSign (Neg a) = natOne
+
 
 -- n -> - n
 intNeg :: Int -> Int
-intNeg = undefined
+intNeg (Pos Zero) = (Pos Zero)
+intNeg (Pos (Succ a)) = (Neg a)
+intNeg (Neg a) = (Pos (Succ a))
+
+natToInt :: Nat -> Nat -> Int
+natToInt Zero a = Pos a
+natToInt a Zero = Pos Zero
+natToInt natOne (Succ a) = Neg (a)
+
 
 -- Дальше также как для натуральных
+triToTri :: Tri -> Tri -> Bool -> Tri
+triToTri EQ EQ True = EQ
+triToTri LT EQ False = GT
+triToTri GT EQ False = LT
+triToTri LT EQ True = LT
+triToTri GT EQ True = GT
+triToTri a GT _ = LT
+triToTri a LT _ = GT
+
+
 intCmp :: Int -> Int -> Tri
-intCmp = undefined
+intCmp n m = triToTri (natCmp (intAbs n) (intAbs m)) (natCmp (intSign n) (intSign m)) (natEq (intSign n) Zero)
+
+
+--intCmp = undefined
+
+triToBool :: Tri -> Tri -> Bool
+triToBool EQ EQ = True
+triToBool LT LT = True
+triToBool GT GT = True
+
+
 
 intEq :: Int -> Int -> Bool
-intEq = undefined
+intEq n m= (if' (triToBool(intCmp n m) EQ) True False)
 
 intLt :: Int -> Int -> Bool
-intLt = undefined
+intLt n m= (if' (triToBool(intCmp n m) LT) True False)
+
 
 infixl 6 .+., .-.
 -- У меня это единственный страшный терм во всём файле
+
+intAdd :: Int -> Int -> Bool -> Int
+intAdd n m True = (natToInt (intSign n) ((intAbs n) +. (intAbs m))) 
+intAdd n m False = (natToInt (intSign n) ((intAbs n) -. (intAbs m))) 
+
+
 (.+.) :: Int -> Int -> Int
-n .+. m = undefined
+n .+. m = intAdd (if' (natLt (intAbs n) (intAbs m)) m n) (if' (natLt (intAbs n) (intAbs m)) n m) (if' (natEq (intSign n) (intSign m)) True False)
 
 (.-.) :: Int -> Int -> Int
 n .-. m = n .+. (intNeg m)
 
 infixl 7 .*.
 (.*.) :: Int -> Int -> Int
-n .*. m = undefined
+n .*. m = natToInt (if' (natEq (intSign n) (intSign m)) Zero natOne) ((intAbs n) *. (intAbs m))
+
 
 -------------------------------------------
 -- Рациональные числа
@@ -176,28 +242,39 @@ ratNeg (Rat x y) = Rat (intNeg x) y
 
 -- У рациональных ещё есть обратные элементы
 ratInv :: Rat -> Rat
-ratInv = undefined
+ratInv (Rat x y)= (Rat (natToInt (intSign x)(y)) ( intAbs x)) 
+
+ratNumb :: Rat -> Int
+ratNumb (Rat x y) = x
+
+ratDec :: Rat -> Int
+ratDec (Rat x y) = Pos y
+
+
 
 -- Дальше как обычно
 ratCmp :: Rat -> Rat -> Tri
-ratCmp = undefined
+ratCmp n m = (if' (ratEq n m) EQ $ (if' (ratLt n m)) LT GT)
 
 ratEq :: Rat -> Rat -> Bool
-ratEq = undefined
+ratEq (Rat x y) (Rat a b)= (intEq x a) && (natEq y b)
 
 ratLt :: Rat -> Rat -> Bool
-ratLt = undefined
+ratLt n m= intLt((ratNumb n) .*. (ratDec m))((ratNumb m) .*. (ratDec n))
+
+norm  :: Rat -> Rat
+norm (Rat n m) = (Rat (natToInt(intSign n)((natDiv (intAbs n)(gcd (intAbs n) (m))))) (natDiv (m) ((gcd (intAbs n) (m)))))
 
 infixl 7 %+, %-
 (%+) :: Rat -> Rat -> Rat
-n %+ m = undefined
+n %+ m = norm (Rat((((ratNumb n) .*. (ratDec m)) .+. ((ratNumb m) .*. (ratDec n)))) (intAbs((ratDec m) .*. (ratDec n))))
 
 (%-) :: Rat -> Rat -> Rat
 n %- m = n %+ (ratNeg m)
 
 infixl 7 %*, %/
 (%*) :: Rat -> Rat -> Rat
-n %* m = undefined
+n %* m = norm (Rat((ratNumb n) .*. (ratNumb m)) (intAbs((ratDec m) .*. (ratDec n))))
 
 (%/) :: Rat -> Rat -> Rat
 n %/ m = n %* (ratInv m)
